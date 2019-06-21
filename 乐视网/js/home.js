683e2fa5a5f418c94f8b4a2b9c81a022f959a19b
Your Name <you@example.com>
2019-06-21 09:43:20 +0800

    跟新

--- 乐视网/js/home.js
+++ 乐视网/js/home.js
@@ -1,3 +1,10 @@
 $(".Promotion-618").click(function(){
    location.href="618.html"
 })
+//搜素点击事件
+$(".txt").focus(function(){
+   $(".sousuo .ipt .txt-box").show()
+})
+$(".txt").blur(function(){
+   $(".sousuo .ipt .txt-box").hide()
+})
